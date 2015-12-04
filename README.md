85b4162328d5fa56fb24e7b28bad90b93978d28f
Jason <jason@hellojason.net>
2015-12-04 17:11:22 -0600

    Updates software list. Adds silent install flag.

--- README.md
+++ README.md
@@ -1,48 +1,10 @@
 # Install applications on fresh instance of Windows
 
-## Included Software
-
-* Atom
-* Clover
-* Firefox
-* Chrome
-* uTorrent
-* Thunderbird
-* -7-zip- (having issues)
-* VLC
-* Spotify
-* Greenshot
-* PuTTY
-* Sublime Text 3
-* WinSCP
-* SourceTree
-* Flux
-* HeidiSQL
-* VirtualBox
-* WPS Office
-
 ## Installation
 
 * run PowerShell on destination machine `as administrator`
-
-* loosen execution policy
-
-```
-Set-ExecutionPolicy Unrestricted
-```
-
-* paste this into powershell
+* paste this command
 
 ```
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/hello-jason/windows-setup/master/setup.ps1'))
 ```
-
-## To do with PowerShell
-
-* Set unrestricted execution policy when calling script
-* Remove desktop shortcuts
-* Pin programs to taskbar
-* Setup desktop theme
-* Setup VM for development
-* Sublime settings
-* Install fonts
--- setup.ps1
+++ setup.ps1
@@ -1,27 +1,43 @@
 # Chocolatey
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
+#############
 # Install applications
-choco install Atom
-choco install Clover
-choco install Firefox
-choco install google-chrome-x64
-choco install uTorrent
-choco install thunderbird
-choco install vlc
-choco install spotify
-choco install greenshot
-choco install putty
-choco install SublimeText3
-choco install SublimeText3.PackageControl 
-choco install winscp
-choco install SourceTree
-choco install f.lux
-choco install git
-choco install HeidiSQL
-choco install virtualbox
-choco install VirtualBox.ExtensionPack
-choco install vagrant
-choco install kingsoft-office-free
+#############
+# Standard
+choco install -y firefox
+choco install -y google-chrome-x64
+choco install -y lightshot
 
+# Media
+choco install -y vlc
+choco install -y spotify
+choco install -y f.lux
+choco install -y putty
+
+# Development tools
+choco install -y atom
+choco install -y cmder
+choco install -y git
+choco install -y heidisql
+choco install -y sublimetext3
+choco install -y sublimetext3.packagecontrol
+choco install -y SourceTree
+choco install -y virtualbox
+choco install -y virtualbox.extensionpack
+choco install -y vagrant
+choco install -y winscp
+
+# Other
+choco install -y ccleaner
+choco install -y uTorrent
+
+#############
+# TODO
+#############
 # Cleanup desktop icons
+# Setup preferences, such as Sublime
+# Pin programs to taskbar
+# Setup desktop theme
+# Setup VM for development
+# Install fonts
